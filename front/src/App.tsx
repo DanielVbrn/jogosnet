3febcda29f0e6a8a5a2fea21e586744940451628
Daniel <danielvbrn@gmail.com>
2025-02-02 04:01:46 -0300

    subindo telas atualizadas do highlight e gamedetails

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,16 +1,21 @@
-import GameDetails from './components/GameDetails/GameDetails';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ProductProvider } from './context/ProductContext';
 import HomePage from './pages/home/HomePage';
-import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import GameDetails from './components/GameDetails/GameDetails';
+import './App.css'; 
+import Header from './components/Header/Header';
 
 export default function App() {
   return (
     <ProductProvider>
-      <Routes>
-        <Route path='/' element={<Navigate to='home' />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/home/game/:id" element={<GameDetails />} />
-      </Routes>
+        <div className="container">
+          <Header/>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/home/game/:id" element={<GameDetails />} />
+          </Routes>
+        </div>
     </ProductProvider>
   );
 }
